7d2eaf1204107d05418fd792c02e596d45edb6bd
audith <audithadhikary081@gmail.com>
2026-04-21 15:08:14 +0600

    argon2 add for hash

--- app/auth.py
+++ app/auth.py
@@ -1,28 +1,31 @@
-from datetime import datetime,timedelta
-from jose import JWTError,jwt
+from datetime import datetime, timedelta
+from jose import jwt
 from passlib.context import CryptContext
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-SECRET_KEY=os.getenv("SECRET_KEY")
-ALGORITHM=os.getenv("ALGORITHM")
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
- 
+
+# ✅ CHANGE HERE: bcrypt → argon2
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 
-pwd_context=CryptContext(schemes=["bcrypt"],deprecated="auto")
-
-def hash_password(password:str):
+def hash_password(password: str):
     return pwd_context.hash(password)
 
-def verify_password(plain,hashed):
-    return pwd_context.verify(plain,hashed)
 
-def create_access_token(data:dict,expires_delta:int=60):
-    to_encode=data.copy()
-    expire=datetime.utcnow()+timedelta(minutes=expires_delta)
-    to_encode.update({"exp":expire})
+def verify_password(plain: str, hashed: str):
+    return pwd_context.verify(plain, hashed)
 
-    return jwt.encode(to_encode,SECRET_KEY,algorithm=ALGORITHM)
+
+def create_access_token(data: dict, expires_delta: int = ACCESS_TOKEN_EXPIRE_MINUTES):
+    to_encode = data.copy()
+
+    expire = datetime.utcnow() + timedelta(minutes=expires_delta)
+    to_encode.update({"exp": expire})
+
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
